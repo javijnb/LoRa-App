6e0e7ca57252c86e4193f00722e742e99d782537
Javier Núñez Bon <javijnb@gmail.com>
2023-06-01 23:41:56 +0200

    sigo odiando frontend pero ahora un poco menos

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <MapContainer center={[42.1698, -8.6879]} zoom={6} scrollWheelZoom={true}>
+    <MapContainer center={[42.1698, -8.6879]} zoom={16} scrollWheelZoom={true}>
       <TileLayer
-        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-        url="'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'"
-      />
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+  />
       <Marker position={[42.1698, -8.6879]}>
         <Popup>
           Escuela Teleco <br /> Sensor 1
