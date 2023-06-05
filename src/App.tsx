8e04b0d033bdc00ec7c0dd7261fb1e54659a7802
Javi Nunez <javijnb@gmail.com>
2023-06-05 16:17:36 +0200

    feat: added sidebar visibility, button and resized components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import MapComponent from './components/Map/Map';
 import Sidebar from './components/Sidebar/Sidebar';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [sidebarVisible, setSidebarVisible] = useState(false)
+
   return (
-    <div className='container'>
-      <Sidebar/>
-      <MapComponent/>
+    <div className='app-container'>
+      {sidebarVisible && <Sidebar />}
+      <div className="map-container">
+        {sidebarVisible ? (
+          <MapComponent style={{ width: '100%' }} sidebarVisible={sidebarVisible} setSidebarVisible={setSidebarVisible}/>
+        ) : (
+          <MapComponent style={{ width: '100vh' }} sidebarVisible={sidebarVisible} setSidebarVisible={setSidebarVisible}/>
+        )}
+      </div>
     </div>
   );
+  
 }
 
 export default App;
