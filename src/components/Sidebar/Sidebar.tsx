6b25ef65bf523cf982d9220ee257297ff2b8026c
Javi Nunez <javijnb@gmail.com>
2023-06-05 16:18:07 +0200

    feat: added more devices to the mock list

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,12 +5,22 @@ interface Device {
   name: string;
 }
 
-const devices: Device[] = [
-  { id: 1, name: 'Dispositivo 1' },
-  { id: 2, name: 'Dispositivo 2' },
-  { id: 3, name: 'Dispositivo 3' },
-  { id: 4, name: 'Dispositivo 4' },
-  { id: 5, name: 'Dispositivo 5' },
+const devices_blancos: Device[] = [
+  { id: 1, name: 'Blanco 1' },
+  { id: 2, name: 'Blanco 2' },
+  { id: 3, name: 'Blanco 3' },
+  { id: 4, name: 'Blanco 4' },
+  { id: 5, name: 'Blanco 5' },
+  { id: 6, name: 'Blanco 6' },
+];
+
+const devices_negros: Device[] = [
+  { id: 1, name: 'Negro 1' },
+  { id: 2, name: 'Negro 2' },
+  { id: 3, name: 'Negro 3' },
+  { id: 4, name: 'Negro 4' },
+  { id: 5, name: 'Negro 5' },
+  { id: 6, name: 'Negro 6' },
 ];
 
 const Sidebar = () => {
@@ -19,7 +29,9 @@ const Sidebar = () => {
         <div className='sidebar'>
         <h2>Lista de dispositivos</h2>
         <ul>
-            {devices.map((device) => (
+            {devices_negros.map((device) => (
+            <li key={device.id}>{device.name}</li>))}
+            {devices_blancos.map((device) => (
             <li key={device.id}>{device.name}</li>))}
         </ul>
         </div>
