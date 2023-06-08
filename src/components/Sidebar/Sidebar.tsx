41df8159634c277a0cc6b0804efa0c7546bfaa9e
Javier Núñez <javijnb@gmail.com>
2023-06-08 17:01:58 +0200

    feat: added color management in sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,39 +3,31 @@ import './Sidebar.css'
 interface Device {
   id: number;
   name: string;
+  color: string;
 }
 
-const devices_blancos: Device[] = [
-  { id: 1, name: 'Blanco 1' },
-  { id: 2, name: 'Blanco 2' },
-  { id: 3, name: 'Blanco 3' },
-  { id: 4, name: 'Blanco 4' },
-  { id: 5, name: 'Blanco 5' },
-  { id: 6, name: 'Blanco 6' },
+const devices: Device[] = [
+  { id: 1, name: 'Blanco' , color: 'white'},
+  { id: 2, name: 'Negro', color: 'black'}
 ];
 
-const devices_negros: Device[] = [
-  { id: 1, name: 'Negro 1' },
-  { id: 2, name: 'Negro 2' },
-  { id: 3, name: 'Negro 3' },
-  { id: 4, name: 'Negro 4' },
-  { id: 5, name: 'Negro 5' },
-  { id: 6, name: 'Negro 6' },
-];
 
-const Sidebar = () => {
+const Sidebar = ({chosen_color = "white"}) => {
 
-    return (
-        <div className='sidebar'>
-        <h2>Lista de dispositivos</h2>
-        <ul>
-            {devices_negros.map((device) => (
-            <li key={device.id}>{device.name}</li>))}
-            {devices_blancos.map((device) => (
-            <li key={device.id}>{device.name}</li>))}
-        </ul>
-        </div>
-    );
+  const handleClick = (color:string) => {
+    chosen_color = color
+    console.log("Nuevo color: ", chosen_color)
+  }
+
+  return (
+    <div className='sidebar'>
+    <h2>Lista de dispositivos</h2>
+    <ul>
+        {devices.map((device) => (
+        <button onClick={() => handleClick(device.color)} key={device.id}>{device.name}</button>))}
+    </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
