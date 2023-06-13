0b78b0068fee75fe4c3f8318c499b8d3ec6f85ab
Javier Núñez <javijnb@gmail.com>
2023-06-13 13:56:25 +0200

    feat: added colors to node buttons

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,42 @@
 import './Sidebar.css'
+import { Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-interface Device {
-  id: number;
-  name: string;
-  color: string;
+const white_theme = createTheme({
+  palette: {
+    white: {
+      main: 'white',
+      contrastText: 'black',
+    }
+  }
+})
+
+const black_theme = createTheme({
+  palette: {
+    black: {
+      main: 'black',
+      contrastText: 'white',
+    }
+  }
+})
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    white: Palette['primary'];
+    black: Palette['primary'];
+  }
+  interface PaletteOptions {
+    white?: PaletteOptions['primary'];
+    black?: PaletteOptions['primary'];
+  }
 }
 
-const devices: Device[] = [
-  { id: 1, name: 'Blanco' , color: 'white'},
-  { id: 2, name: 'Negro', color: 'black'}
-];
-
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    white: true;
+    black: true;
+  }
+}
 
 const Sidebar = ({node_color, setNodeColor}:any) => {
 
@@ -20,12 +46,16 @@ const Sidebar = ({node_color, setNodeColor}:any) => {
   }
 
   return (
+    
     <div className='sidebar'>
     <h2>Lista de dispositivos</h2>
-    <ul>
-        {devices.map((device) => (
-        <button onClick={() => handleClick(device.color)} key={device.id}>{device.name}</button>))}
-    </ul>
+      <ThemeProvider theme={white_theme}>
+        <Button color="white" size="medium" variant='contained' onClick={() => handleClick('white')}>Dispositivo Blanco </Button>
+      </ThemeProvider>
+        <br/><br/>
+      <ThemeProvider theme={black_theme}>
+        <Button color="black" size="medium" variant='contained' onClick={() => handleClick('black')}>Dispositivo Negro </Button>
+      </ThemeProvider>
     </div>
   );
 };
